915e4313ff83837fa231ad639fd72cce786dc50f
Thongrapee <kulaspa@gmail.com>
2016-08-11 10:16:48 +0700

    Add route for getting all students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,10 @@
 class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+    render :json => @students
+  end
+
   def assign
     binding.pry
     ActionCable.server.broadcast 'students', 'Hallo, guten Morgen'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     mount ActionCable.server => '/live'
+    get '/students' => 'students#index'
     post '/students/assign' => 'students#assign'
 end
