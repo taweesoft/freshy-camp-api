84d9adffbea036060b3bdd8e748cd6605e0702da
TAWEESOFT <taweesoft@gmail.com>
2016-12-11 15:03:53 +0700

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Group.create(color:'red', limit: 15)
+Group.create(color:'orange', limit: 15)
+Group.create(color:'yellow', limit: 15)
+Group.create(color:'brown', limit: 15)
+Group.create(color:'blue', limit: 15)
+Group.create(color:'green', limit: 15)
